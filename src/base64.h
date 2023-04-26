e318fee5f00f415d32cd253a216dbc728031f272
Robert <131800313+sorcgiver@users.noreply.github.com>
2023-04-26 15:01:08 +0400

    Update base64.h

--- src/base64.h
+++ src/base64.h
@@ -9,8 +9,8 @@ extern "C" {
 //    void* GetBase64EncodeSize(const unsigned int data_in_size, unsigned int *data_out_size);
 //    void* GetBase64DecodeSize(const unsigned char *data_in, const unsigned int data_in_size, unsigned int *data_out_size);
 //    unsigned char GetBase64Code(const unsigned char symbol);
-    void base64_encode(const unsigned char* data_in, const unsigned int data_in_size, unsigned char** LPdata_out, unsigned int *data_out_size); // encrypt in base64 binary
-    void base64_decode(const unsigned char* data_in, const unsigned int data_in_size, unsigned char** LPdata_out, unsigned int *data_out_size); // decrypt in base64 binary
+    void* base64_encode(const unsigned char* data_in, const unsigned int data_in_size, unsigned int *data_out_size); // encrypt in base64 binary
+    void* base64_decode(const unsigned char* data_in, const unsigned int data_in_size, unsigned int *data_out_size); // decrypt in base64 binary
     char* base64_encode_str(char* data); // encrypt in base64 string
     char* base64_decode_str(char* data); // decrypt in base64 string
 
@@ -45,22 +45,20 @@ extern "C" {
  * 
  * 
  * ======================== encrypt in base64 binary ===========================
- * void base64_encode(const unsigned char* data_in, const unsigned int data_in_size, unsigned char** LPdata_out, unsigned int *data_out_size);
+ * void* base64_encode(const unsigned char* data_in, const unsigned int data_in_size, unsigned int *data_out_size);
  *      data_in -       input byte array
  *      data_in_size -  size input byte array
- *      LPdata_out -    output encrypted array base64 (malloc)
  *      data_out_size - size output encrypted array base64 without '\0' (you can NULL)
- * RETURN: void
+ * RETURN: output encrypted array base64 (malloc)
  * WARNING: do not forget to clear the memory LPdata_out
  * 
  * 
  * ======================== decrypt in base64 binary ===========================
- * void base64_decode(const unsigned char* data_in, const unsigned int data_in_size, unsigned char** LPdata_out, unsigned int *data_out_size);
+ * void* base64_decode(const unsigned char* data_in, const unsigned int data_in_size, unsigned int *data_out_size);
  *      data_in -       input base64 encrypted array
  *      data_in_size -  size input base64 encrypted array
- *      LPdata_out -    output decoded array (malloc)
  *      data_out_size - size output decoded array without '\0' (you can NULL)
- * RETURN: void
+ * RETURN: output decoded array (malloc)
  * WARNING: do not forget to clear the memory LPdata_out
  * 
  * 
