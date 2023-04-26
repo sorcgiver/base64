8405b9fc7123160c92788380fd410a291c5755c7
Robert <131800313+sorcgiver@users.noreply.github.com>
2023-04-26 15:00:19 +0400

    Update base64.c

--- src/base64.c
+++ src/base64.c
@@ -54,71 +54,71 @@ unsigned char GetBase64Code(const unsigned char symbol) {
         return 64;
 }
 
-void base64_encode(const unsigned char* data_in, const unsigned int data_in_size, unsigned char** LPdata_out, unsigned int *data_out_size) {
+void* base64_encode(const unsigned char* data_in, const unsigned int data_in_size, unsigned int *data_out_size) {
+    unsigned char* LPdata_out;
     int i, j;
     unsigned int col;
     unsigned int out_size;
-    *LPdata_out = GetBase64EncodeSize(data_in_size, &out_size);
-    (*LPdata_out)[out_size] = '\0';
+    LPdata_out = GetBase64EncodeSize(data_in_size, &out_size);
+    LPdata_out[out_size] = '\0';
     if (data_in_size % 3 != 0)
         col = (out_size) - 4;
     else
         col = out_size;
     for (i = 0, j = 0; i < col; i += 4, j += 3) {
-        (*LPdata_out)[i] = base64_code[data_in[j] >> 2];
-        (*LPdata_out)[i + 1] = base64_code[((data_in[j] & 0x03) << 4) | ((data_in[j + 1] & 0xf0) >> 4)];
-        (*LPdata_out)[i + 2] = base64_code[((data_in[j + 1] & 0x0f) << 2) | ((data_in[j + 2] & 0xc0) >> 6)];
-        (*LPdata_out)[i + 3] = base64_code[data_in[j + 2] & 0x3f];
+        LPdata_out[i] = base64_code[data_in[j] >> 2];
+        LPdata_out[i + 1] = base64_code[((data_in[j] & 0x03) << 4) | ((data_in[j + 1] & 0xf0) >> 4)];
+        LPdata_out[i + 2] = base64_code[((data_in[j + 1] & 0x0f) << 2) | ((data_in[j + 2] & 0xc0) >> 6)];
+        LPdata_out[i + 3] = base64_code[data_in[j + 2] & 0x3f];
     }
     if (col != out_size) {
-        (*LPdata_out)[i] = base64_code[data_in[j] >> 2];
+        LPdata_out[i] = base64_code[data_in[j] >> 2];
         if (j + 1 < data_in_size) {
-            (*LPdata_out)[i + 1] = base64_code[((data_in[j] & 0x03) << 4) | ((data_in[j + 1] & 0xf0) >> 4)];
-            (*LPdata_out)[i + 2] = base64_code[((data_in[j + 1] & 0x0f) << 2)];
+            LPdata_out[i + 1] = base64_code[((data_in[j] & 0x03) << 4) | ((data_in[j + 1] & 0xf0) >> 4)];
+            LPdata_out[i + 2] = base64_code[((data_in[j + 1] & 0x0f) << 2)];
         } else {
-            (*LPdata_out)[i + 1] = base64_code[((data_in[j] & 0x03) << 4)];
-            (*LPdata_out)[i + 2] = base64_code[64];
+            LPdata_out[i + 1] = base64_code[((data_in[j] & 0x03) << 4)];
+            LPdata_out[i + 2] = base64_code[64];
         }
-        (*LPdata_out)[i + 3] = base64_code[64];
+        LPdata_out[i + 3] = base64_code[64];
     }
     if (data_out_size != NULL)
         *data_out_size = out_size;
+    return LPdata_out;
 }
 
-void base64_decode(const unsigned char* data_in, const unsigned int data_in_size, unsigned char** LPdata_out, unsigned int *data_out_size) {
+void* base64_decode(const unsigned char* data_in, const unsigned int data_in_size, unsigned int *data_out_size) {
+    unsigned char* LPdata_out;
     int i, j, z;
     unsigned int col;
     unsigned char bc[4];
     unsigned int out_size;
-    *LPdata_out = GetBase64DecodeSize(data_in, data_in_size, &out_size);
-    (*LPdata_out)[out_size] = '\0';
+    LPdata_out = GetBase64DecodeSize(data_in, data_in_size, &out_size);
+    LPdata_out[out_size] = '\0';
     col = (out_size) / 3 * 3;
     for (i = 0, j = 0; i < col; i += 3, j += 4) {
         for (z = 0; z < 4; z++)
             bc[z] = GetBase64Code(data_in[j + z]);
-        (*LPdata_out)[i] = (bc[0] << 2) | (bc[1] >> 4);
-        (*LPdata_out)[i + 1] = (bc[1] << 4) | (bc[2] >> 2);
-        (*LPdata_out)[i + 2] = (bc[2] << 6) | bc[3];
+        LPdata_out[i] = (bc[0] << 2) | (bc[1] >> 4);
+        LPdata_out[i + 1] = (bc[1] << 4) | (bc[2] >> 2);
+        LPdata_out[i + 2] = (bc[2] << 6) | bc[3];
     }
     if (i < out_size) {
         for (z = 0; z < 3; z++)
             bc[z] = GetBase64Code(data_in[j + z]);
-        (*LPdata_out)[i] = (bc[0] << 2) | (bc[1] >> 4);
+        LPdata_out[i] = (bc[0] << 2) | (bc[1] >> 4);
         if (i + 1 < out_size)
-            (*LPdata_out)[i + 1] = (bc[1] << 4) | (bc[2] >> 2);
+            LPdata_out[i + 1] = (bc[1] << 4) | (bc[2] >> 2);
     }
     if (data_out_size != NULL)
         *data_out_size = out_size;
+    return LPdata_out;
 }
 
 char* base64_encode_str(char* data) {
-    unsigned char *ret;
-    base64_encode((unsigned char*) data, strlen(data), &ret, NULL);
-    return (char*) ret;
+    return base64_encode((unsigned char*) data, strlen(data), NULL);
 }
 
 char* base64_decode_str(char* data) {
-    unsigned char *ret;
-    base64_decode((unsigned char*) data, strlen(data), &ret, NULL);
-    return (char*) ret;
+    return base64_decode((unsigned char*) data, strlen(data), NULL);
 }
